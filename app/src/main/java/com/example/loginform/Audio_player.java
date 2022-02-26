946b456737a5ba60a0c37a5902fa8513ba30293e
butt <salmanrajpoot919@gmail.com>
2022-02-26 08:00:49 -0800

    Audio.Player

--- app/src/main/java/com/example/loginform/Audio_player.java
+++ app/src/main/java/com/example/loginform/Audio_player.java
@@ -10,5 +10,6 @@ public class Audio_player extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_player);
+    getSupportActionBar().hide();
     }
 }
