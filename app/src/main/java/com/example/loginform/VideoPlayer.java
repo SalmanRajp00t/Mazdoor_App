8d2fb3b22df1956b52c9d721ee1e755dde6bd92c
butt <salmanrajpoot919@gmail.com>
2022-02-28 07:07:34 -0800

    View.Player completed

---
+++ app/src/main/java/com/example/loginform/VideoPlayer.java
@@ -0,0 +1,25 @@
+package com.example.loginform;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VideoPlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+
+        VideoView videoView = findViewById(R.id.taarevideo);
+        String videoPath = "android.resource://" + getPackageName()+"/"+R.raw.taarevideo;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+    }
+}
