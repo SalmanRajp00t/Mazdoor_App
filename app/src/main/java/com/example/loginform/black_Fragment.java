5e5f179e1df2669aab891f390a48780f9423fda6
sad <salmanrajpoot919@gmail.com>
2022-04-26 16:57:22 -0700

    try

--- app/src/main/java/com/example/loginform/black_Fragment.java
+++ app/src/main/java/com/example/loginform/black_Fragment.java
@@ -14,6 +14,8 @@ public class black_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+
+
         return inflater.inflate(R.layout.fragment_black_, container, false);
     }
 }
