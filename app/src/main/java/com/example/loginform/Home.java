c093d70444ea3244ee30e45d2b6891d4b2e44a5a
sad <salmanrajpoot919@gmail.com>
2022-04-28 02:15:28 -0700

    home page text design

--- app/src/main/java/com/example/loginform/Home.java
+++ app/src/main/java/com/example/loginform/Home.java
@@ -1,14 +1,47 @@
 package com.example.loginform;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Home extends AppCompatActivity {
+
+
+    BottomNavigationView bottomNavigationView;
+     black_Fragment blackFragment=new black_Fragment();
+     blue_Fragment blueFragment =new blue_Fragment();
+     green_Fragment greenFragment = new green_Fragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,blackFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.black:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,blackFragment).commit();
+                    return true;
+                case R.id.blue:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,blueFragment).commit();
+                    return true;
+                case R.id.green:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,greenFragment).commit();
+                    return true;
+            }
+
+            return false;
+        });
+        getSupportActionBar().hide();
     }
 }
